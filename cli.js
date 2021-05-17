badbc92c0cb755e2a187e0bd643a2ca30ebc8ceb
Araxeus <oaeben@gmail.com>
2021-05-17 04:26:03 +0300

    minify

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 let filePath;
+let savePath;
 
 run();
 
@@ -9,34 +10,44 @@ async function run() {
     //process.argv -> [1]=location launched [2]=appended arg
     try {
         filePath = path.join(process.cwd(), process.argv[2]);
-    } catch { error() }
+    } catch { sendError() }
+
+    savePath = process.argv.length < 4 ?
+        filePath :
+        filePath.slice(0, filePath.lastIndexOf(path.sep) + 1) + process.argv[3]
 
     console.log(`Reading File at ${filePath}`)
-    fs.readFile(filePath, "utf8", function (err, data) {
-        if (err) error();
+    fs.readFile(filePath, "utf8", function (error, data) {
+        if (error) sendError(1);
 
-        // code formatter
-        // ```javascript -> {% highlight javascript %}
-        data = data.replace(/\`\`\`(.*)/g, "{% highlight $1 %}");
-        data = data.replace(/```/g, "{% endhighlight %}")
-
-        //use html <a><img></a> instead of markdown [![randomName](imageUrl)](clickLink)
-        data = data.replace(/\[!\[.*\]\((.*)\)]\((.*)\)/g, '<a href ="$2"><img src="$1"></a>')
-
-        //use html <img> instead of markdown ![]()
-        data = data.replace(/!\[.*\]\((.*)\)/g, '<img src="$1">')
-
-        //use raw instead of blob
-        //https://github.com/Araxeus/mark2jek/blob/pics/ok.png -> https://raw.githubusercontent.com/Araxeus/mark2jek/ok.png
-        data = data.replace(/(?:https|http):\/\/github.com\/(.*)\/blob/g, "https://raw.githubusercontent.com/$1")
-
-        fs.writeFile(filePath, data, () =>
-            console.log(`Succesfully Ran mark2jek on ${filePath}`)
-        );
+        fs.writeFile(savePath, doRegex(data), (err) => {
+            if (err) sendError(2);
+            if (savePath !== filePath) {
+                console.log(`Succesfully saved mark2jek output to ${savePath}`)
+            } else {
+                console.log(`Succesfully ran mark2jek on ${filePath}`)
+            }
+        });
     })
 }
 
-function error() {
-    console.error("cant load file at: " + (filePath || process.argv[2] || "UNDEFINED"));
-    process.exit(1);
+function doRegex(data) {
+    return data
+        .replace(/\`\`\`(.*)/g, "{% highlight $1 %}")
+        .replace(/```/g, "{% endhighlight %}")
+        .replace(/\[!\[.*\]\((.*)\)]\((.*)\)/g, '<a href ="$2"><img src="$1"></a>')
+        .replace(/!\[.*\]\((.*)\)/g, '<img src="$1">')
+        .replace(/(?:https|http):\/\/github.com\/(.*)\/blob/g, "https://raw.githubusercontent.com/$1")
+}
+
+function sendError(errorCode) {
+    switch (errorCode) {
+        case 1:
+            console.error("cant load file at: " + (filePath || process.argv[2] || "UNDEFINED"));
+            break;
+        case 2:
+            console.error("cant save file to " + (savePath || process.argv[3] || "UNDEFINED"));
+            break;
+    }
+    process.exit(errorCode);
 }
